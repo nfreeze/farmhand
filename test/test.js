8217683930f48261cffb62d50d49b24d65f75d27
nathan <nathan@freezable.com>
2013-06-09 20:54:32 -0500

    initial commit

---
+++ test/test.js
@@ -0,0 +1,35 @@
+var FarmHand = require('../lib/farmhand.js');
+
+var ping = function(){
+    var i = 0;
+    var self = this;
+    var interval = setInterval(function(){
+        i += 1;
+        if (self.cancel){
+            console.log('cancelling worker');
+            clearInterval(interval);
+        }
+        console.log('scope var:',self.scopevar);
+        self.progress({percent:i,time:Date.now().toString()});
+    },1000);
+};
+
+var farmhand = new FarmHand(ping,null,{scopevar: 'testing scope'});
+farmhand.on('progress',function(state){
+  console.log('farmhand progress:',state);
+});
+
+farmhand.run(function(err,result){
+    if (err){
+        console.log('farmhand error:',err);
+    }
+    console.log('farmhand result: ',result);
+});
+
+setInterval(function(){
+    console.log('main interval');
+},1000);
+
+setTimeout(function(){
+    farmhand.cancel();
+},10000);
